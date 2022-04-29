927b72317b6e0defabf0ef0ac083be3ce6b0b58e
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-04-29 17:37:58 +0100

    Add App ui and delete some unwanted files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// eslint-disable-next-line no-unused-vars
+import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  render() {
+    return (
+      <div className="App">
+        <div className="appContainer">
+          <h1>Welcome, Math Magicians!</h1>
+          <Calculator />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
